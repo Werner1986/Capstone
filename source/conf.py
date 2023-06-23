13bdc262a492a83ec916547d9f9518632df223c4
Werner1986 <wiwdzcm@gmail.com>
2023-06-23 03:26:09 +0200

    Changed the project name

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'website'
+project = 'websiteproject'
 copyright = '2023, Werner'
 author = 'Werner'
 
